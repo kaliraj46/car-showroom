c71ca467e7238cb5b7d2cf540c0406fac15bd94f
kaliraj46 <720822103077@hit.edu.in>
2024-12-01 16:16:51 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Example of a simple interaction when the user clicks on a button
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function() {
+        alert('More details coming soon!');
+    });
+});
